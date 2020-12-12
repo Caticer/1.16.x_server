821520ff3ca5a65a50868e28976cfd38f0343d6b
Caticer <doopthe@gmail.com>
2020-12-12 19:35:32 +0300

    ChestCommands update. v3.1.2 to 4.0.3

---
+++ start - 1.16.2.bat
@@ -0,0 +1,3 @@
+@echo off
+java -Xmx1G -jar -Dfile.encoding=UTF-8 spigot-1.16.3.jar nogui
+pause
---
+++ start - 1.16.4.bat
@@ -0,0 +1,4 @@
+@echo off
+echo minecraft 1.16.4 still under testing!
+java -Xmx1G -jar -Dfile.encoding=UTF-8 spigot-1.16.4.jar nogui
+pause
