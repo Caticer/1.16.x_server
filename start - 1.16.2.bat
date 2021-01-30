23aeac4f7432f9f1978e842dd2dec79b5e3b6bf8
Caticer <doopthe@gmail.com>
2021-01-30 17:42:13 +0300

    cleaning

--- start - 1.16.2.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-java -Xmx1G -jar -Dfile.encoding=UTF-8 spigot-1.16.3.jar nogui
-pause
--- start - 1.16.4.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Warning: minecraft 1.16.4 still under testing! use olders versions if you have issues
-java -Xmx1G -jar -Dfile.encoding=UTF-8 spigot-1.16.4.jar nogui
-echo Server stoped!
-pause
